47ce2b469cc7e1bd216894b411a049bd53ba143c
Brandon20202 <brandon.taylor@austinisd.org>
2020-05-28 21:28:59 -0500

    added values

---
+++ Inventory.sql
@@ -0,0 +1,5 @@
+INSERT into products VALUES (1,AirsoftGun,Sports/Outdoors,35.88,10);
+INSERT into prducts VAlUES (2,XboxOne,VideoGames,159.24,4);
+INSERT into products VALUES (3,PS4,VideoGames,39.68, 4);
+INSERT into products VALUES (4,tv36in ,Electronics, 85, 2);
+INSERT into products VALUES (5,bananas,food,.65,50);
